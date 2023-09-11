99ba043b19b937beec73e75ff2e1f8f760de5c75
xFooFoo <73238233+xFooFoo@users.noreply.github.com>
2023-09-11 15:18:03 +1200

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,23 @@
+from typing import List
+import random
+
+def insertion_sort(data: List[str]):
+    n = len(data)
+    print(data)
+    for i in range(1,n):
+        for j in range(i, 0, -1):
+            if data[j-1] > data[j]:
+                data[j], data[j-1] = data[j-1], data[j]
+                print(data)
+
+# Generate a list of 10 random numbers
+random_numbers = [random.randint(1, 1000) for _ in range(10)]
+
+insertion_sort(random_numbers)
+output = random_numbers
+#Test whether algorithm sorts properly
+if output == sorted(random_numbers):
+    print("The list has been sorted.")
+else:
+    print(output)
+    print(sorted(random_numbers))
---
+++ selection_sort.py
@@ -0,0 +1,28 @@
+from typing import List
+import random
+
+def selection_sort(data: List[int]):
+    n = len(data)
+    print(data)
+    for i in range(1,n):
+        min_tup = (i-1,data[i-1]) #key,val in a tuple
+        for j in range(i,n): #last iteration n to n ignored since it must be the largest left over in the last position
+            if data[j] < min_tup[1]:
+                min_tup = (j, data[j]) #minimum value in unsorterd position
+        data[min_tup[0]], data[i-1] = data[i-1] ,min_tup[1] #swap value of the ith position with the minimum value
+        print(data)
+
+
+# Generate a list of 10 random numbers
+random_numbers = [random.randint(1, 100) for _ in range(10)]
+
+selection_sort(random_numbers)
+output = random_numbers
+#Test whether algorithm sorts properly
+if output == sorted(random_numbers):
+    print("The list has been sorted.")
+else:
+    print(output)
+    print(sorted(random_numbers))
+        
+    
